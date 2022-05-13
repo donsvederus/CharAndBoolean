b3048dea767bcdfc13c29a07ea7c2cfad9c10457
Don Svederus <svederus@gmail.com>
2022-05-13 00:03:54 -0600

    0.031 The char and boolean Primitive Types

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -17,5 +17,6 @@ public class Main {
         System.out.println(myFalseBooleanValue);
 
         boolean isCustomerOverTwentyOne = true;
+        System.out.println(isCustomerOverTwentyOne);
     }
 }
