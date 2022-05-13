3046a76edc286847546c8ab1809c3f47d21bf1b9
Don Svederus <svederus@gmail.com>
2022-05-13 00:02:13 -0600

    0.031 The char and boolean Primitive Types

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -4,5 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // char used to store a single character in 16 bits
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        System.out.println(myTrueBooleanValue);
+        System.out.println(myFalseBooleanValue);
+
+        boolean isCustomerOverTwentyOne = true;
     }
 }
